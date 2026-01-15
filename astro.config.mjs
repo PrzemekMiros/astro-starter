48f1f9676245050e358ef5f457abfc2cbb44d980
PrzemekMiros <kontakt@futurewebstudio.pl>
2026-01-15 18:47:59 +0100

    Site title

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import netlify from '@astrojs/netlify';
 import tailwindcss from '@tailwindcss/vite';
 import robots from 'astro-robots-txt';
 import sitemap from 'astro-sitemap';
+import min from 'astro-min';
 
 export default defineConfig({
   site: 'https://astrokeystat.netlify.app/',
@@ -19,7 +20,8 @@ export default defineConfig({
         { userAgent: '*', allow: '/' }
       ],
       sitemap: 'https://astrokeystat.netlify.app/sitemap.xml'
-    })
+    }),
+    min()
   ],
 
   vite: {
